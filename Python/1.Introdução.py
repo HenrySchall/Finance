e12ca71edfffd091123c6bb5127e660c6ee22858
Henrique Werner <henrique.schall@outlook.com>
2024-07-09 04:53:46 -0300

    .

--- Python/1.Introdução.py
+++ Python/1.Introdução.py
@@ -27,7 +27,7 @@ dataset = ['VALE3.SA', 'WEGE3.SA', 'ITUB4.SA', 'BOVA11.SA']
 # Criando um vetor de ativos
 dataset_vector = pd.DataFrame()
 for acao in dataset:
-  dataset_vector[acao] = yf.download(acao, start='2015-01-02')['Close']
+  dataset_vector[acao] = yf.download(acao, start='2015-01-02', end=None)['Close']
 dataset_vector
 
 ########################
@@ -60,15 +60,13 @@ plt.show()
 
 # gráfico preços comprados 
 dataset_df.plot(xlabel='Date', figsize = (10,7), title = 'Histórico do preço das ações')
-plt.show()
-
-figura = px.line(title = 'Comparativo carteira x BOVA')
+plt.show() 
 
 ###########################
 ### Taxa de Crescimento ### 
 ###########################
 
-# A Taxa de crescimento reflete o quanto estamos multiplicamos nosso capital sobre o valor da data de compra. Sendo assim, para calcularmos ela usaremos os valores dos ativos normalizados, 
+# A Taxa de Crescimento reflete o quanto estamos multiplicamos nosso capital sobre o valor da data de compra. Para calcularmos ela usaremos os valores dos ativos normalizados, 
 # ou seja, dividindo o preço de fechamento de cada dia, pelo preço inicial de compra.
 
 dataset_df_normalizado = dataset_df.copy()
@@ -87,70 +85,93 @@ plt.show()
 
 # Verificando Valores
 dataset = dataset_df
-dataset
-dataset['VALE'][0], dataset['VALE'][2289]
+
+len(dataset) 
+dataset['VALE'][0], dataset['VALE'][2338] # contagem começa do zero, por isso volta-se 1 período)
+dataset['VALE'][0], dataset['VALE'][len(dataset)-1]
 
 ###############
 ### Período ###
 ###############
 
 # Retorno Vale 
-((dataset['VALE'][2289] - dataset['VALE'][0]) / dataset['VALE'][0]) * 100
+((dataset['VALE'][2338] - dataset['VALE'][0]) / dataset['VALE'][0]) * 100
+((dataset['VALE'][len(dataset)-1] - dataset['VALE'][0]) / dataset['VALE'][0]) * 100
 # Retorno Wege 
-((dataset['WEGE'][2289]- dataset['WEGE'][0]) / dataset['WEGE'][0]) * 100
+((dataset['WEGE'][len(dataset)-1] - dataset['WEGE'][0]) / dataset['WEGE'][0]) * 100
 # Retorno Itaú 
-((dataset['ITAU'][2289] - dataset['ITAU'][0]) / dataset['ITAU'][0]) * 100
+((dataset['ITAU'][len(dataset)-1] - dataset['ITAU'][0]) / dataset['ITAU'][0]) * 100
 
-# Resultados:
-# Vale = 192.38%
-# Wege = 562.15%
-# Itau = 72.80%
+# Resultados: (valores considerando end.date 09/07/2024)
+# Vale = 196.52%
+# Wege = 650.79%
+# Itau = 76.34%
 
-##############
-### Diário ###
-##############
+######################
+### Retorno Diário ###
+######################
 
 dataset['WEGE']
-dataset['WEGE'].shift(1)
+dataset['WEGE'].shift(1) # pega a posição anterior
 
 # Retorno Wege
-dataset['RS Wege'] = (dataset['WEGE'] / dataset['WEGE'].shift(1)) - 1
-dataset['RS Wege']
+dataset['RS Diário Wege'] = (dataset['WEGE'] / dataset['WEGE'].shift(1)) - 1
+dataset['RS Diário Wege']
 
-dataset['RS Wege'].plot()
+dataset['RS Diário Wege'].plot()
 plt.show()
 
 # Retorno Itaú
-dataset['RS Itau'] = (dataset['ITAU'] / dataset['ITAU'].shift(1)) - 1
-dataset['RS Itau']
+dataset['RS Diário Itau'] = (dataset['ITAU'] / dataset['ITAU'].shift(1)) - 1
+dataset['RS Diário Itau']
 
-dataset['RS Itau'].plot()
+dataset['RS Diário Itau'].plot()
 plt.show()
 
 # Retorno Vale
-dataset['RS Vale'] = (dataset['VALE'] / dataset['VALE'].shift(1)) - 1
-dataset['RS Vale']
+dataset['RS Diário Vale'] = (dataset['VALE'] / dataset['VALE'].shift(1)) - 1
+dataset['RS Diário Vale']
 
-dataset['RS Vale'].plot()
+dataset['RS Diário Vale'].plot()
 plt.show()
 
-#############
-### Anual ###
-#############
+dataset
+
+#########################
+### Retorno Semestral ###
+#########################
 
 # Retorno Wege
-(dataset['RS Wege'].mean() * 246) * 100
+(dataset['RS Diário Wege'].mean() * 126) * 100
 
 # Retorno Itaú
-(dataset['RS Itau'].mean() * 246) * 100
+(dataset['RS Diário Itau'].mean() * 126) * 100
 
 # Retorno Anual
-(dataset['RS Vale'].mean() * 246) * 100
+(dataset['RS Diário Vale'].mean() * 126) * 100
 
 # Resultados:
-# Vale = 21.51%
-# Wege = 26.01%
-# Itau = 10.89%
+# Vale = 13.73%
+# Wege = 5.58%
+# Itau = 10.87%
+
+#####################
+### Retorno Anual ###
+#####################
+
+# Retorno Wege
+(dataset['RS Diário Wege'].mean() * 252) * 100
+
+# Retorno Itaú
+(dataset['RS Diário Itau'].mean() * 252) * 100
+
+# Retorno Anual
+(dataset['RS Diário Vale'].mean() * 252) * 100
+
+# Resultados:
+# Vale = 27.47%
+# Wege = 11.17%
+# Itau = 21.75%
 
 ###################################
 ### Taxa de Retorno Logaritmica ### 
@@ -158,17 +179,35 @@ plt.show()
 
 # Usa-se para comparar a MESMA ação em períodos diferentes
 
-# Fixando datas
-dataset
-dataset.iloc[[1216]] # 02/01/2020
-dataset.iloc[[1217]] # 03/01/2020
-dataset.iloc[[2210]] # 03/01/2024
+np.log(dataset['VALE'][len(dataset) - 1]/dataset['VALE'][0]) * 100
+dataset['RL Diário Vale'] = np.log(dataset['VALE']/ dataset['VALE'].shift(1))
 
-# Criando dataset do período 1
-dataset.vale = ['VALE3.SA']
+(dataset['RL Diário Vale'].mean() * 252) * 100
+
+np.log(dataset['WEGE'][len(dataset) - 1]/dataset['WEGE'][0]) * 100
+dataset['RL Diário Wege'] = np.log(dataset['WEGE'][len(dataset) - 1] / dataset['WEGE'].shift(1))
+
+(dataset['RL Diário Wege'].mean() * 252) * 100
+
+np.log(dataset['ITAU'][len(dataset) - 1]/dataset['ITAU'][0]) * 100
+dataset['RL Diário Itau'] = np.log(dataset['ITAU'][len(dataset) - 1] / dataset['ITAU'].shift(1))
+
+(dataset['RL Diário Itau'].mean() * 252) * 100
+
+dataset
+
+#######################
+### Exemplo Prático ###
+####################### 
+
+vale = ['VALE3.SA']
+
+#############################
+### Retorno Logaritmico 1 ###
+#############################
 
 dataset_vector = pd.DataFrame()
-for acao in dataset.vale:
+for acao in vale:
   dataset_vector[acao] = yf.download(acao, start='2015-01-02', end='2020-01-03')['Close']
 dataset_vector 
 
@@ -176,11 +215,22 @@ dataset_log = dataset_vector.rename(columns={'VALE3.SA': 'VALE'})
 dataset_log.isnull().sum()
 dataset_log
 
-# Criando dataset do período 2
-dataset.vale = ['VALE3.SA']
+# Retorno do Período 
+np.log(dataset_log['VALE'][len(dataset_log)-1]/dataset_log['VALE'][0]) * 100
+
+# Retorno do Diário
+dataset_log['RL VALE'] = np.log(dataset_log['VALE'] / dataset_log['VALE'].shift(1))
+dataset_log
+
+# Retorno do Anual 
+(dataset_log['RL Diário VALE'].mean() * 252) * 100
+
+#############################
+### Retorno Logaritmico 2 ###
+#############################
 
 dataset_vector = pd.DataFrame()
-for acao in dataset.vale:
+for acao in vale:
   dataset_vector[acao] = yf.download(acao, start='2020-01-03', end='2024-01-04')['Close']
 dataset_vector 
 
@@ -188,133 +238,103 @@ dataset_log2 = dataset_vector.rename(columns={'VALE3.SA': 'VALE'})
 dataset_log2.isnull().sum()
 dataset_log2
 
-#############################
-### Retorno Logaritmico 1 ###
-#############################
-
 # Retorno do Período 
-np.log(dataset_log['VALE'][1216]/dataset_log['VALE'][0]) * 100
+np.log(dataset['VALE'][len(dataset_log)-1]/dataset['VALE'][0]) * 100
 
 # Retorno do Diário
-dataset_log['RL VALE'] = np.log(dataset_log['VALE'] / dataset_log['VALE'].shift(1))
-dataset_log
-
-# Retorno do Anual 
-(dataset_log['RL VALE'].mean() * 246) * 100
-
-#############################
-### Retorno Logaritmico 2 ###
-#############################
-
-# Retorno do Período 
-np.log(dataset['VALE'][2210]/dataset['VALE'][1218]) * 100
-
-# Retorno do Diário
-dataset_log2['RL VALE'] = np.log(dataset_log2['VALE'] / dataset_log2['VALE'].shift(1))
+dataset_log2['RL Diário VALE'] = np.log(dataset_log2['VALE'] / dataset_log2['VALE'].shift(1))
 dataset_log2
 
 # Retorno do Anual 
-(dataset_log2['RL VALE'].mean() * 246) * 100
+(dataset_log2['RL VALE'].mean() * 252) * 100
 
 # Resultado Período 1:
-# Período = 81.69%
-# Anual = 18.55%
+# Período = 93.73%
+# Anual = 19.00%
 
 # Resultado Período 2:
-# Período = 35.15%
-# Anual = 8.70%
+# Período = 85.80%
+# Anual = 8.92%
 
-#############################
-### Retorno Logaritmico 3 ###
-#############################
+# Observa-se que o Período 1 obteve melhores resultados do que o Período 2
 
-np.log(dataset['VALE'][2289]/dataset['VALE'][0]) * 100
-dataset['RL Vale'] = np.log(dataset['VALE'] / dataset['VALE'].shift(1))
-(dataset['RL Vale'].mean() * 246) * 100
+###########################
+### Retorno da Carteira ###
+###########################
 
-np.log(dataset['WEGE'][2289]/dataset['WEGE'][0]) * 100
-dataset['RL Wege'] = np.log(dataset['WEGE'] / dataset['WEGE'].shift(1))
-(dataset['RL Wege'].mean() * 246) * 100
+# Reiniciando o dataset (Rodando novamente, sem o calculo dos retornos)
+dataset2 = ['VALE3.SA', 'WEGE3.SA', 'ITUB4.SA', 'BOVA11.SA']
 
-np.log(dataset['ITAU'][2289]/dataset['ITAU'][0]) * 100
-dataset['RL Itau'] = np.log(dataset['ITAU'] / dataset['ITAU'].shift(1))
-(dataset['RL Itau'].mean() * 246) * 100
+dataset_vector = pd.DataFrame()
+for acao in dataset2:
+  dataset_vector[acao] = yf.download(acao, start='2015-01-02', end=None)['Close']
+dataset_vector
 
-dataset
+dataset_df2 = dataset_vector.rename(columns={'VALE3.SA': 'VALE', 'WEGE3.SA': 'WEGE','ITUB4.SA': 'ITAU', 'BOVA11.SA': 'BOVA'})
+dataset_df2
 
-####################################
-### Retorno de carteira de ações ###
-####################################
+dataset_df2.isnull().sum()
+dataset_df2.dropna(inplace=True)
+dataset_df2.isnull().sum()
 
-# Relembrando os dados do dataframe normalizado
-dataset_df_normalizado
-dataset_df_normalizado.plot(xlabel = 'Date', figsize=(15, 7))
-plt.show()
-
-# Apagando a coluna date
-dataset_df_normalizado = dataset_df_normalizado.reset_index()
-dataset_df_normalizado.drop(labels=['Date'], axis=1, inplace=True)
+dataset_df_normalizado = dataset_df2.copy()
+for i in dataset_df_normalizado.columns[0:]:
+  dataset_df_normalizado[i] = dataset_df_normalizado[i] / dataset_df_normalizado[i][0]
 dataset_df_normalizado
 
-###################################
-### Retorno carteira individual ###
-###################################
+################################
+### Retorno individual Ativo ###
+################################
 
 # Retorno do período
 retorno_carteira = (dataset_df_normalizado / dataset_df_normalizado.shift(1)) - 1
 retorno_carteira.head()
 
 # Retorno anual de cada ativo (%)
-retorno_anual = (retorno_carteira.mean() * 246 ) * 100
-retorno_anual
+retorno_anual = (retorno_carteira.mean() * 252) * 100
+retorno_anual # mesmos valores encontrados anteriormente
 
-#################################
-### Retorno carteira conjunta ###
-#################################
+########################
+### Retorno carteira ### 
+########################
 
-# Calculando os pesos 
+# Definindo os Pesos de cada Ativo
 pesos_carteira = np.array([0.33, 0.33, 0.34, 0.0])
-pesos_carteira.sum()
+pesos_carteira.sum() # 1.0 = 100%
 
 # Retorno Carteira 
 np.dot(retorno_anual, pesos_carteira)
-retorno_anual
+
+# Retorno da Carteira -> 20,04%
+# Retorno do BOVA11 -> 13.40%
 
 #######################
 ### Carteira X IBOV ###
 #######################
 
-dataset_normalizado = dataset_df.copy()
-for i in dataset_normalizado.columns[0:]:
-  dataset_normalizado[i] = (dataset_normalizado[i] / dataset_normalizado[i][0])
-dataset_normalizado.head()
+dataset_df_normalizado = dataset_df2.copy()
+for i in dataset_df_normalizado.columns[0:]:
+  dataset_df_normalizado[i] = dataset_df_normalizado[i] / dataset_df_normalizado[i][0]
+dataset_df_normalizado
 
-# Limpando colunas não necessárias
-dataset_normalizado.drop(labels=['RL Vale'], axis=1, inplace=True)
-dataset_normalizado.drop(labels=['RL Itau'], axis=1, inplace=True)
-dataset_normalizado.drop(labels=['RL Wege'], axis=1, inplace=True)
-dataset_normalizado.drop(labels=['RS Vale'], axis=1, inplace=True)
-dataset_normalizado.drop(labels=['RS Itau'], axis=1, inplace=True)
-dataset_normalizado.drop(labels=['RS Wege'], axis=1, inplace=True)
-dataset_normalizado = dataset_normalizado.reset_index()
+dataset_df_normalizado['CARTEIRA'] = (dataset_df_normalizado['VALE'] + dataset_df_normalizado['ITAU'] + dataset_df_normalizado['WEGE']) / 3
+dataset_df_normalizado
 
-dataset_normalizado.head()
-
-dataset_normalizado['CARTEIRA'] = (dataset_normalizado['VALE'] + dataset_normalizado['ITAU'] + dataset_normalizado['WEGE']) / 3
-dataset_normalizado
+dataset_df_normalizado = dataset_df_normalizado.reset_index()
 
 figura = px.line(title = 'Comparativo carteira x BOVA')
-for i in dataset_normalizado.columns[1:]:
-  figura.add_scatter(x = dataset_normalizado['Date'], y = dataset_normalizado[i], name = i)
+for i in dataset_df_normalizado.columns[1:]:
+  figura.add_scatter(x = dataset_df_normalizado['Date'], y = dataset_df_normalizado[i], name = i)
 figura.show()
 
-dataset_normalizado.drop(['VALE', 'WEGE', 'ITAU'], axis = 1, inplace= True)
-dataset_normalizado
+# Limpando colunas não necessárias
+dataset_df_normalizado.drop(['VALE', 'WEGE', 'ITAU', 'Date'], axis = 1, inplace= True)
+dataset_df_normalizado
 
 figura = px.line(title = 'Comparativo carteira x BOVA')
-for i in dataset_normalizado.columns[1:]:
-  figura.add_scatter(x = dataset_normalizado['Date'], y = dataset_normalizado[i], name = i)
+for i in dataset_df_normalizado.columns[1:]:
+  figura.add_scatter(x = dataset_df_normalizado['Date'], y = dataset_df_normalizado[i], name = i)
 figura.show()
 
 # Exportar como CSV
-dataset_df.to_csv('dataset_df.csv')
+dataset_df_normalizado.to_csv('dataset_df.csv')
